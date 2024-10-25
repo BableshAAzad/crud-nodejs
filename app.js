a335ae642db808f0766dbe687f165dafbbd513d4
bableshaazad <bableshaazad@outllok.com>
2024-10-25 14:05:21 +0530

    update user by id done👶😷

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.get("/find-by-email", async (req, resp) => {
   if (user) {
     resp.status(200).send(user)
   } else {
-    resp.status(500).send({ "Message": "User Not Found "})
+    resp.status(500).send({ "Message": "User Not Found " })
   }
 })
 
@@ -32,6 +32,31 @@ app.post("/save", async (req, resp) => {
   try {
     await user.save();
     resp.status(201).send({ "status": "success", "message": "Registration Success" })
+  } catch (error) {
+    resp.status(500).send({ "status": "failed", "message": "Unable to Register" })
+  }
+})
+
+
+app.post("/update-user/:userId", async (req, resp) => {
+  let { email, name, password, address } = req.body
+  let { userId } = req.params
+  console.log(userId)
+  let user = User.findById(userId)
+  if (user) {
+    await User.findByIdAndUpdate(userId, {
+      $set: {
+        name: name,
+        email: email,
+        password: password,
+        address: address
+      }
+    })
+    resp.status(200).send({ "status": "success", "message": "Update done Success" })
+  } else {
+
+  }
+  try {
   } catch (error) {
     resp.status(500).send({ "status": "failed", "message": "Unable to Register" })
   }
